4eb98758974441654cb7059502b26af3b9c14975
yyyqyyy <yyyqyyy@hotmail.com>
2017-10-01 18:02:13 +0800

    321

--- mine/static/js/mine.js
+++ mine/static/js/mine.js
@@ -1,3 +1,15 @@
 /**
  * Created by yuqy on 2017/9/29.
  */
+var mineGame = {
+    layers: [],
+    currentLevel: 1,
+    width: 400,
+    height: 400,
+    mines: 20,
+    range: {
+        rows: 8,
+        columns: 8
+    },
+    blockWidth: 50
+};
