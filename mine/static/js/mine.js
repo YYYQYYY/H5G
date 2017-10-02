40ec3cd918b510f559642c7553404e3a3fddaf05
yyyqyyy <yyyqyyy@hotmail.com>
2017-10-02 14:19:44 +0800

    000

--- mine/static/js/mine.js
+++ mine/static/js/mine.js
@@ -20,6 +20,12 @@ mineGame.levels = [
     }
 ];
 
+function Cell() {
+    this.data = 0;
+    this.isOpened = false;
+    this.isFlag = false;
+}
+
 /**
  * 随机生成地址
  * @returns {*[]}
@@ -47,7 +53,7 @@ function initDataMap() {
     var currentGame = mineGame.levels[mineGame.currentLevel];
     for (var r = 0; r < currentGame.range.rows; r++) {
         mineGame.dataMap[r] = Array.apply(null, Array(currentGame.range.columns)).map(function (i) {
-            return 0;
+            return new Cell();
         });
     }
 }
@@ -65,7 +71,7 @@ function createMines() {
         var key = pos.x + "=" + pos.y;
         if (!tempArr[key]) {
             tempArr[key] = 1;
-            mineGame.dataMap[pos.x][pos.y] = -1;
+            mineGame.dataMap[pos.x][pos.y].data = -1;
             scanAroundCell(pos);
             mineCount--;
         }
@@ -86,8 +92,8 @@ function scanAroundCell(pos) {
         if (r > -1 && r < rows) {
             for (var ci = -1; ci < 2; ci++) {
                 var c = pos.y + ci;
-                if (c > -1 && c < columns && !(r == pos.x && c == pos.y) && mineGame.dataMap[r][c] > -1) {
-                    mineGame.dataMap[r][c]++;
+                if (c > -1 && c < columns && !(r == pos.x && c == pos.y) && mineGame.dataMap[r][c].data > -1) {
+                    mineGame.dataMap[r][c].data++;
                 }
             }
         }
@@ -101,7 +107,16 @@ function scanAroundCell(pos) {
  * 初始化游戏画布
  */
 function initLayers() {
-
+    mineGame.layers.forEach(function (i) {
+            //mineGame.layers[i].canvas.oncontextmenu = function (e) {
+            //    if (document.all) {
+            //        window.event.returnValue = false;
+            //    } else {
+            //        event.preventDefault();
+            //    }
+            //}
+        }
+    );
 }
 
 /**
@@ -150,12 +165,12 @@ function drawDataMap() {
     var msg = "";
     for (var ri = 0; ri < rows; ri++) {
         for (var ci = 0; ci < columns; ci++) {
-            if (mineGame.dataMap[ri][ci] < 0) {
+            if (mineGame.dataMap[ri][ci].data < 0) {
                 ctx.fillStyle = "lightgray";
                 msg = "M";
-            } else if (mineGame.dataMap[ri][ci] > 0) {
+            } else if (mineGame.dataMap[ri][ci].data > 0) {
                 ctx.fillStyle = "blue";
-                msg = mineGame.dataMap[ri][ci];
+                msg = mineGame.dataMap[ri][ci].data;
             } else {
                 ctx.fillStyle = "green";
                 msg = "";
@@ -167,35 +182,94 @@ function drawDataMap() {
     }
 }
 
+function drawMask() {
+    var currentGame = mineGame.levels[mineGame.currentLevel];
+    var cellWidth = currentGame.cellWidth;
+    var rows = currentGame.range.rows;
+    var columns = currentGame.range.columns;
+    var ctx = mineGame.layers[1];
+
+    for (var ri = 0; ri < rows; ri++) {
+        for (var ci = 0; ci < columns; ci++) {
+            ctx.fillStyle = "lightblue";
+            ctx.fillRect(ri * cellWidth + 1, ci * cellWidth + 1, cellWidth - 1, cellWidth - 1);
+        }
+    }
+}
+
+function openCell(x, y) {
+    var currentGame = mineGame.levels[mineGame.currentLevel];
+    var cellWidth = currentGame.cellWidth;
+    var ri = Math.floor(x / cellWidth);
+    var ci = Math.floor(y / cellWidth);
+
+    if (!mineGame.dataMap[ri][ci].isOpened && !mineGame.dataMap[ri][ci].isFlag) {
+        var ctx = mineGame.layers[1];
+        ctx.clearRect(ri * cellWidth + 1, ci * cellWidth + 1, cellWidth - 1, cellWidth - 1);
+        mineGame.dataMap[ri][ci].isOpened = true;
+    }
+}
+
+function drawFlag(x, y) {
+    var currentGame = mineGame.levels[mineGame.currentLevel];
+    var cellWidth = currentGame.cellWidth;
+    var ri = Math.floor(x / cellWidth);
+    var ci = Math.floor(y / cellWidth);
+    var ctx = mineGame.layers[1];
+
+    if (mineGame.dataMap[ri][ci].isOpened) {
+        return;
+    }
+
+    if (!mineGame.dataMap[ri][ci].isFlag) {
+        ctx.fillStyle = "#abc";
+        ctx.fillRect(ri * cellWidth + cellWidth / 4, ci * cellWidth + cellWidth / 4, cellWidth / 2 - 1, cellWidth / 2 - 1);
+    } else {
+        ctx.clearRect(ri * cellWidth + cellWidth / 4, ci * cellWidth + cellWidth / 4, cellWidth / 2 - 1, cellWidth / 2 - 1);
+        ctx.fillStyle = "lightblue";
+        ctx.fillRect(ri * cellWidth + cellWidth / 4 - 1, ci * cellWidth + cellWidth / 4 - 1, cellWidth / 2, cellWidth / 2 + 1);
+    }
+    mineGame.dataMap[ri][ci].isFlag = !mineGame.dataMap[ri][ci].isFlag;
+}
+
 function gameLoop() {
+}
+
+function disableRightClick(e) {
+    if (document.all) {
+        window.event.returnValue = false;
+    } else {
+        event.preventDefault();
+    }
 }
 
 $(function () {
     var canvas_bg = document.getElementById("bg");
     mineGame.layers[0] = canvas_bg.getContext("2d");
+    canvas_bg.oncontextmenu = disableRightClick;
     var canvas_game = document.getElementById("game");
     mineGame.layers[1] = canvas_game.getContext("2d");
+    canvas_game.oncontextmenu = disableRightClick;
 
     createGameData();
 
     initLayers();
 
     var layers = $("#layers");
-    //layers.mousedown(function (e) {
-    //    var canvasPosition = $(this).offset();
-    //    var mouseX = (e.pageX - canvasPosition.left) || 0;
-    //    var mouseY = (e.pageY - canvasPosition.top) || 0;
-    //    for (var i = 0; i < mineGame.circles.length; i++) {
-    //        var circleX = mineGame.circles[i].x;
-    //        var circleY = mineGame.circles[i].y;
-    //        var radius = mineGame.circles[i].radius;
-    //        if (Math.pow(mouseX - circleX, 2) + Math.pow(mouseY - circleY, 2) < Math.pow(radius, 2)) {
-    //            mineGame.targetCircle = i;
-    //            break;
-    //        }
-    //    }
-    //});
-    //
+    layers.oncontextmenu = disableRightClick;
+
+    layers.mousedown(function (e) {
+        var canvasPosition = $(this).offset();
+        var mouseX = (e.pageX - canvasPosition.left) || 0;
+        var mouseY = (e.pageY - canvasPosition.top) || 0;
+
+        if (event.button == 0) {
+            openCell(mouseX, mouseY);
+        } else {
+            drawFlag(mouseX, mouseY);
+        }
+    });
+
     //layers.mousemove(function (e) {
     //    if (mineGame.targetCircle != undefined) {
     //        var canvasPosition = $(this).offset();
@@ -208,18 +282,17 @@ $(function () {
     //    updateLineIntersection();
     //    updateLevelProgress();
     //});
-    //
-    //layers.mouseup(function (e) {
-    //    mineGame.targetCircle = undefined;
-    //
-    //    checkLevelCompleteness();
-    //});
+
+    layers.mouseup(function (e) {
+        //checkLevelCompleteness();
+    });
 
     //setupCurrentLevel();
 
     //mineGame.background = new Image();
     //mineGame.background.onload = function () {
     drawGridCells();
+    drawMask();
     setInterval(gameLoop, 30);
     //};
     //mineGame.background.onerror = function () {
