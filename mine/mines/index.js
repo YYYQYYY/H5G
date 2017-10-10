6e0313bec36ae05bd1ff75f28fdc908d836bcc6a
yyyqyyy <yyyqyyy@hotmail.com>
2017-10-10 09:18:20 +0800

    move

---
+++ mine/mines/index.js
@@ -0,0 +1,25 @@
+var app = require('express')()
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server);
+
+var port = process.env.PORT || 1337;
+
+// ms s
+var module = require("./mines");
+var mineServer = new module.MineServer();
+mineServer.SetConfig({
+    "ListenPort": port,
+    "RoomTotal": 100,
+    "MaxClientNum": 300
+});
+mineServer.Startup(io);
+// ms e
+
+server.listen(port);
+
+app.get('/', function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello. Almc.");
+});
+
+console.log("Server running at http://localhost:%d", port);
