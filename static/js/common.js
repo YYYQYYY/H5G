c8b8137cd28ab5704896cf5d0a128fed9798573b
yyyqyyy <yyyqyyy@hotmail.com>
2017-10-14 10:50:10 +0800

    456456

--- static/js/common.js
+++ static/js/common.js
@@ -7,6 +7,21 @@ Object.freeze(CONST);
 
 var isPC, isWeixin, isAndroid, isIos, WW, WH;
 
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth > 750) clientWidth = 750;//这里限制最大的宽度尺寸，从而实现PC端的两边留白等
+            docEl.style.fontSize = 10 * (clientWidth / 320) + 'px';
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
 $(function () {
     isPC = !navigator.userAgent.match(/mobile/i);
     var ua = navigator.userAgent.toLowerCase();
